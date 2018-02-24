0a08f917e021d4e8aab82beaa223f12d4ddbedde
Angelo Scalabroni <armotek5421@msn.com>
2018-02-24 16:54:05 -0500

    set up the basic scaffolding with bootstrap, after reverting npm-webpack attempt

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
+
+/**
+ * Register Gameboard component globally
+ */
+Vue.component('gameboard', {
+    template: '#gameboard-template'
+});
+
+
+/**
+ * Register Scoreboard component globally
+ */
+Vue.component('scoreboard', {
+    template: '#scoreboard-template', //TODO: Loop through players' names and display their names and scores here in the list-
+});
+
+/**
+ * Register StatusControls component globally
+ */
+Vue.component('statuscontrols', {
+    template: '#statuscontrols-template' //TODO: Display who's turn it is, player 1 or player 2
+});
+
 new Vue({
-  el: '#app',
-  data: {
-    message: 'This is the message.. vue is loading, working'
-  }
+    el: '#app',
+    data: {
+        message: 'This is the message.. vue is loading, working'
+    }
 })
