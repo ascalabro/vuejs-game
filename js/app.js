4c09941440b0a7331438677be0e456d579ec4206
Angelo Scalabroni <armotek5421@msn.com>
2018-02-25 12:43:07 -0500

    WIP tic tac toe game functionality working; now to update scoreboard after every game using localStorage

--- js/app.js
+++ js/app.js
@@ -3,7 +3,50 @@
  * Register Gameboard component globally
  */
 Vue.component('gameboard', {
-    template: '#gameboard-template'
+    template: '#gameboard-template',
+    data: {},
+    methods: {
+        playAttempt: function (event) {
+            var cellValue = event.target.innerHTML,
+                cellId = event.target.getAttribute('cell-id');
+            if (cellValue == '') { // Empty cell
+                console.log('The cell was empty...' + cellValue);
+                currentPlayerToken = this.$root.$data.store.currentPlayer;
+                event.target.innerHTML = currentPlayerToken; // Put the player token into the grid
+                this.$root.$data.store.playerPositions[currentPlayerToken].push(cellId); // Keep track of which cells the player occupies
+                this.alternatePlayerTurn(); // Only alternate player if a move was made
+            } else { // Not empty, player already moved here
+                console.log('The cell was NOT empty: ' + cellValue);
+                return; // Do nothing
+            }
+            var currentPlayerPositions = this.$root.$data.store.playerPositions[currentPlayerToken];
+            if (this.isWinner(currentPlayerPositions)) {
+                alert('Player ' + currentPlayerToken + " has won this game");
+            }
+
+            // `this` inside methods points to the Vue instance
+            // `event` is the native DOM event
+            if (event) {
+                // alert(event.target.innerHTML)
+            }
+        },
+        alternatePlayerTurn() {
+            var playerTokens = this.$root.$data.store.playerTokens;
+            for (i = 0; i < playerTokens.length; i++) {
+                if (this.$root.$data.store.currentPlayer == playerTokens[i]) {
+                    continue;
+                } else {
+                    this.$root.$data.store.currentPlayer = playerTokens[i];
+                    break;
+                }
+            }
+        },
+        isWinner(positions) {
+             const arr = this.$root.$data.store.winningCellCombinations;
+             return arr.some(combination => combination.every(n => positions.includes(n)))
+        },
+
+    }
 });
 
 
@@ -12,18 +55,50 @@ Vue.component('gameboard', {
  */
 Vue.component('scoreboard', {
     template: '#scoreboard-template', //TODO: Loop through players' names and display their names and scores here in the list-
+    methods: {
+        getBestScore() {
+            return localStorage.getItem("localHighScore");
+        },
+        setBestScore(score) {
+            localStorage.setItem("localHighScore", score);
+        }
+    }
 });
 
 /**
  * Register StatusControls component globally
  */
 Vue.component('statuscontrols', {
-    template: '#statuscontrols-template' //TODO: Display who's turn it is, player 1 or player 2
+    template: '#statuscontrols-template',
+    methods: {
+        getActivePlayer() {
+            return this.$root.$data.store.currentPlayer;
+        }
+    }
 });
 
-new Vue({
+// Shared data
+let store = {
+    playerTokens: [
+        'X', 'O'
+    ],
+    currentPlayer: 'X',
+    playerPositions: {
+        'X': [],
+        'O': [],
+    },
+    winningCellCombinations: [
+        ["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"],
+        ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"],
+        ["1", "5", "9"], ["3", "5", "7"]
+    ]
+};
+
+
+var vm = new Vue({
     el: '#app',
-    data: {
-        message: 'This is the message.. vue is loading, working'
+    data: { // get this with this.$root.$data.message
+        message: 'This is a $root var called `message`...',
+        store: store
     }
-})
+});
